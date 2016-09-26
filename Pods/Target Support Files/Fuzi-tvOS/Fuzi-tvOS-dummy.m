9bcb6399d152d222b5ff672b4645e852af03b334
Jonas Zaugg <jonas.zaugg@yahoo.com>
2016-09-26 23:42:44 +0200

    Refactored all schemes for multi-platform support

---
+++ Pods/Target Support Files/Fuzi-tvOS/Fuzi-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Fuzi_tvOS : NSObject
+@end
+@implementation PodsDummy_Fuzi_tvOS
+@end
---
+++ Pods/Target Support Files/Pods-AlamoFuzi.tvOS/Pods-AlamoFuzi.tvOS-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AlamoFuzi_tvOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AlamoFuzi_tvOSVersionString[];
+
