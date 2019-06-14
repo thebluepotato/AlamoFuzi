80c0463941fbc4d0a804ca1bb55097455ff94f54
Jonas Zaugg <jonas.zaugg@yahoo.com>
2019-06-14 23:16:05 +0200

    SPM

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.1
+
+import PackageDescription
+let package = Package(
+    name: "AlamoFuzi",
+    products: [
+        .library(
+            name: "AlamoFuzi",
+            targets: ["AlamoFuzi"]),
+    ],
+    dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-beta.1"),
+                   .package(url: "https://github.com/cezheng/Fuzi.git", from: "2.0.0")],
+    targets: [
+        .target(
+            name: "AlamoFuzi", path: "Sources", dependencies: ["Alamofire", "Fuzi"])
+            .testTarget(name: "AlamoFuziTests", path: "Tests")
+    ]
+)
