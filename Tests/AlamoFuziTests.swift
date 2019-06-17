366ebb0fbeff4198ebd678c98a97f41d29849572
Jonas Zaugg <jonas.zaugg@yahoo.com>
2019-06-17 10:33:00 +0200

    Update tests for Travis

--- Tests/AlamoFuziTests.swift
+++ Tests/AlamoFuziTests.swift
@@ -28,11 +28,21 @@ class AlamoFuziTests: XCTestCase {
         
         XCTAssertNil(response.error, "Request for example HTML page failed with error: \(response.error!)")
         
-        let html = try XCTUnwrap(response.value, "Response result value is nil.")
-        
-        let tag = try XCTUnwrap(html.firstChild(css: "div > p")?.stringValue, "The example HTML file does not have the expected structure.")
-        
-        XCTAssertEqual(tag, "This domain is established to be used for illustrative examples in documents. You may use this\n    domain in examples without prior coordination or asking for permission.", "The HTML did not have the expected content")
+        if #available(iOS 13.0, *) { // For XCTUnwrap
+            let html = try XCTUnwrap(response.value, "Response result value is nil.")
+            
+            let tag = try XCTUnwrap(html.firstChild(css: "div > p")?.stringValue, "The example HTML file does not have the expected structure.")
+            
+            XCTAssertEqual(tag, "This domain is established to be used for illustrative examples in documents. You may use this\n    domain in examples without prior coordination or asking for permission.", "The HTML did not have the expected content")
+        } else {
+            let html = response.value
+            XCTAssertNotNil(html, "Response result value is nil.")
+            
+            let tag = html?.firstChild(css: "div > p")?.stringValue
+            XCTAssertNotNil(tag, "The example HTML file does not have the expected structure.")
+            
+            XCTAssertEqual(tag, "This domain is established to be used for illustrative examples in documents. You may use this\n    domain in examples without prior coordination or asking for permission.", "The HTML did not have the expected content")
+        }
     }
     
     func testXMLResponse() throws {
@@ -47,10 +57,20 @@ class AlamoFuziTests: XCTestCase {
         
         XCTAssertNil(response.error, "Request for example XML page failed with error: \(response.error!)")
         
-        let xml = try XCTUnwrap(response.value, "Response result value is nil.")
-        
-        let tag = try XCTUnwrap(xml.firstChild(xpath: "//note/from")?.stringValue, "The example XML file does not have the expected structure.")
-        
-        XCTAssertEqual(tag, "Jani", "The HTML did not have the expected content")
+        if #available(iOS 13.0, *) { // For XCTUnwrap
+            let xml = try XCTUnwrap(response.value, "Response result value is nil.")
+            
+            let tag = try XCTUnwrap(xml.firstChild(xpath: "//note/from")?.stringValue, "The example XML file does not have the expected structure.")
+            
+            XCTAssertEqual(tag, "Jani", "The XML did not have the expected content")
+        } else {
+            let xml = response.value
+            XCTAssertNotNil(xml, "Response result value is nil.")
+            
+            let tag = xml?.firstChild(xpath: "//note/from")?.stringValue
+            XCTAssertNotNil(tag, "The example XML file does not have the expected structure.")
+            
+            XCTAssertEqual(tag, "Jani", "The XML did not have the expected content")
+        }
     }
 }
